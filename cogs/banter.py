02ef917dfb37c80ba1bae7a1d676510b35d5a300
Zayn Khan <93350342+Zeke07@users.noreply.github.com>
2022-06-13 00:02:17 -0500

    v1.1 '/banter add' created

--- cogs/banter.py
+++ cogs/banter.py
@@ -1,11 +1,14 @@
 '''
-Work-in-progress
-(i.e. did not get to it yet because I really wanted to write /henle)
-
+Zeke Bot - Banter cog for generating taunts directed at specific users
+Author: Zayn Khan
+Version Date: 6-12-2022
+Email: zaynalikhan@gmail.com
 '''
 import discord
 from discord.ext import commands
 from discord import app_commands
+import database
+import miscellaneous
 
 
 class Banter(commands.GroupCog, group_name="banter"):
@@ -13,9 +16,39 @@ class Banter(commands.GroupCog, group_name="banter"):
         super().__init__()
         self.bot=bot
 
+
+    # /banter add 
+    # add a taunt directed at a specific user to the server database
     @app_commands.command(name="add", description="add text taunts")
-    async def add(self, interaction:discord.Interaction):
-        await interaction.response.send_message("hi")
+    async def add(self, interaction:discord.Interaction, target_user: str, taunt: str):
+        ''' code structure for sending banter (just an aesthetic thing)
+        taunt=self.generate_taunt(target=target,taunt=taunt)
+        if type(taunt)==discord.Embed:
+            await interaction.response.send_message(embed=taunt)
+        else:
+            await interaction.response.send_message(taunt)
+        '''
+
+        if (not target_user.startswith("<@")):
+            await interaction.response.send_message('`Target user must have an @`')
+            return
+        if (len(taunt)>4096):
+            await interaction.response.send_message('`Taunt is too long (longer than 4096 characters) or be a url`')
+
+        server_id=interaction.guild_id
+        mem_id=target_user[3:len(target_user)-1]
+        miscellaneous.initialize_banters(server_id, mem_id)
+
+        # add taunt to list in database keyed by user's id
+        # the DB 'banter' key in the document stores keys of user_ids, each referring to
+        # a list full of taunts directed at that specific user
+        database.add_instance(server_id)
+        database.db[server_id]['banter'][mem_id].append(taunt)
+        to_store=database.db[server_id]['banter']
+        database.store_data(key='banter',guild_id=server_id,data=to_store)
+
+        await interaction.response.send_message(f'`Updated banter for` {target_user}')
+
 
 
 async def setup(bot: commands.Bot):

936779c7261db6876bd622b2fc014c548f22c890
Zayn Khan <93350342+Zeke07@users.noreply.github.com>
2022-06-24 20:29:02 -0500

    moved class 'Interact' from henle.py

--- miscellaneous.py
+++ miscellaneous.py
@@ -2,10 +2,12 @@
 Zeke Bot - Class for miscellaneous tasks such as checking for the existence of data in the server document
 or initializing data for the functionality of specific cogs if missing
 Author: Zayn Khan
-Version Date: 6-16-2022
+Version Date: 6-12-2022
 Email: zaynalikhan@gmail.com
 
 '''
+import discord
+
 import database
 import validators
 
@@ -32,3 +34,40 @@ def generate_taunt(target: str, taunt: str):
         return f'<@{target}>: {taunt}'
     else:
         return f'<@{target}>: `{taunt}`'
+
+
+# Overwritten discord view for ease of access in cogs
+class Interact(discord.ui.View):
+    def __init__(self, num_pages, embeds: [discord.Embed], timeout):
+        super().__init__()
+        self.pages=num_pages
+        # the search results will usually have multiple pages, and we want to update the message with
+        # a discord embed at a new given index in the array depending on if they hit back or next
+        self.embed_pos=0
+        self.embeds=embeds
+        self.timeout=timeout
+
+
+    async def on_timeout(self):
+            await self.message.delete()
+            self.stop()
+
+    @discord.ui.button(label="Back", style=discord.ButtonStyle.gray)
+    async def previous(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if (self.embed_pos - 1 >= 0): # move back a page, if applicable
+            self.embed_pos = self.embed_pos - 1
+            await self.message.edit(embed=self.embeds[self.embed_pos])
+        await interaction.response.defer()
+
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.green)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if (self.embed_pos + 1 < self.pages): # move up a page, if applicable
+            self.embed_pos = self.embed_pos + 1
+            await self.message.edit(embed=self.embeds[self.embed_pos])
+        await interaction.response.defer()
+
+    @discord.ui.button(label="Exit", style=discord.ButtonStyle.danger)
+    async def exit(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("`Exiting search...`")
+        await self.message.delete()
+        self.stop()

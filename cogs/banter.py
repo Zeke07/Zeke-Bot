c8840eebb7c61f532a581583b0bfbe2c15e7988d
Zayn Khan <93350342+Zeke07@users.noreply.github.com>
2022-06-26 02:41:26 -0500

    save progress on '/banter remove'

--- cogs/banter.py
+++ cogs/banter.py
@@ -57,6 +57,62 @@ class Banter(commands.GroupCog, group_name="banter"):
         database.store_data(key='banter',guild_id=server_id,data=to_store)
 
         await interaction.response.send_message(f'`Updated banter for` {target_user}')
+        
+    # INCOMPLETE, selection menu soon to be added to properly implement removal function
+    # /banter remove
+    # display or remove taunts for a specific user
+    @app_commands.command(name="remove", description="add text taunts")
+    async def remove(self, interaction: discord.Interaction, target_user: str):
+        if (not target_user.startswith("<@")):
+            await interaction.response.send_message('`Target user must have an @`')
+            return
+
+        server_id = interaction.guild_id
+        mem_id = target_user[3:len(target_user) - 1]
+        print(mem_id)
+        banter=database.db[server_id]['banter']
+        if not mem_id in banter.keys():
+            await interaction.response.send_message("`Target user has no taunts to display, use '/banter add' to create one`")
+            return
+
+        member = self.bot.get_user(int(mem_id))
+        to_display=banter[mem_id]
+        embeds=[]
+        descriptions=[""]
+        index=0
+        count=1
+        single_page=True
+        for taunt in to_display:
+            if (len(descriptions[index]+taunt)<=4096):
+                descriptions[index]+=f'\n`{count}. {taunt}`'
+                count+=1
+            else:
+                descriptions.append("")
+                index+=1
+                descriptions[index]+=f'\n`{count}. {taunt}`'
+                count+=1
+        pages=len(descriptions)
+        count=1
+        for desc in descriptions:
+            embeds.append(discord.Embed(title=f"`Banter for {member.name}#{member.discriminator}`", description=desc, colour=discord.Colour.dark_orange()).set_footer(text=f'Page {count}/{pages}'))
+            count+=1
+       
+        #await interaction.response.send_message(embed=embeds[0])
+        #await interaction.followup.send(embed=embeds[1])
+
+        '''
+                   description=""
+                   if (len(taunt+description)<=5):
+                       description+=f'\n`{count}. {taunt}`'
+                       count+=1
+                   else:
+                       single_page=False
+                       embeds.append(discord.Embed(title=f"`Banter for {member.name}#{member.discriminator}`", description=description))
+                       description=""
+
+               if single_page:
+                   embeds.append(discord.Embed(title=f"`Banter for {member.name}#{member.discriminator}`", description=description))
+        '''
 
 
 
